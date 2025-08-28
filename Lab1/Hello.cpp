a402d9821cfa0285b8d7a9113f2a5fd4fb7fdbdc
MaximoSoto <supersmartmaximo2005@icloud.com>
2025-08-28 12:19:17 -0700

    The work for Lab 1 in Object Oriented Programming

---
+++ Lab1/Hello.cpp
@@ -0,0 +1,13 @@
+/***
+ * Familiarize myself with CLI and compile and execute C++ programs
+ ***/
+
+ #include <iostream>
+ #include <cstdlib>
+ using namespace std;
+
+ int main() {
+    cout << "Hello World!" << endl;
+    printf("Hello World again!\n");
+    return 0;
+ }
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Object-Oriented-Programming
-
+![alt text](<Deliverable 2.jpg>)
+![alt text](<Deliverable 3.jpg>)
