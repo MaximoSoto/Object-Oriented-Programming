25a7c333d81d7a72c6293c4b188da66c9af12ba1
MaximoSoto <supersmartmaximo2005@icloud.com>
2025-12-04 02:01:16 -0800

    Completion of all needed code for Group Project, along with image of updated UML in image folder

--- GroupProject/main.cpp
+++ GroupProject/main.cpp
@@ -3,18 +3,23 @@
 #include <vector>
 #include "TelemetryRecord.h"
 #include "Input.h"
+#include "Hub.h"
 using namespace std;
 
 int main()
 {
     TelemetryRecord T1 = TelemetryRecord();
+    Hub H1 = Hub();
     loadDataFromFile("iot_telemetry_data.csv", T1);
 
-    for(int i = 0; i < T1.get_size(); i++){
-        cout << i + 1 << " " << T1.get_ts(i) << "  " << T1.get_device(i) << "  " << T1.get_co(i) << "  " << T1.get_humidity(i) << "  " << (T1.get_light(i) ? "TRUE" : "FALSE") << "  " << T1.get_lpg(i) << "   " << (T1.get_motion(i) ? "TRUE" : "FALSE") << "  " << T1.get_smoke(i) << "  " << T1.get_temp(i) << endl;
-        if(T1.get_motion(i) == 1){
-            break;
-        }
-    }
-    cout << endl << T1.get_size();
+    H1.convertTemp(T1);
+
+    H1.writeCoToCsv("output_co.csv", T1);
+    H1.writeHumidityToCsv("output_humidity.csv", T1);
+    H1.writeLightToCsv("output_light.csv", T1);
+    H1.writeLpgToCsv("output_lpg.csv", T1);
+    H1.writeMotionToCsv("output_motion.csv", T1);
+    H1.writeSmokeToCsv("output_smoke.csv", T1);
+    H1.writeTempToCsv("output_temp.csv", T1);
+    
 }
